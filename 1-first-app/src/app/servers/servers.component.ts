c7fbe230a9508162718878f7dccec5d2c350d7e9
melodiaz23 <meli.diaz23@gmail.com>
2023-06-08 15:57:49 -0500

    learning directives

--- 1-first-app/src/app/servers/servers.component.ts
+++ 1-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,8 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
   constructor(){
     setTimeout(() => {
@@ -20,6 +22,8 @@ export class ServersComponent {
   }
 
   onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
