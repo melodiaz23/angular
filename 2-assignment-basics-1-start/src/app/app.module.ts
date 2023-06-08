fef191b5618c43aa8028c174466dcb6e4423c622
melodiaz23 <meli.diaz23@gmail.com>
2023-06-08 08:47:24 -0500

    task finished

--- 2-assignment-basics-1-start/src/app/app.module.ts
+++ 2-assignment-basics-1-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { WarningAlert } from './warningAlert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ 2-assignment-basics-1-start/src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'warning-alert',
+  templateUrl: './warningAlert.component.html',
+  styleUrls: ['./warningAlert.component.css'],
+})
+export class WarningAlert{
+}
