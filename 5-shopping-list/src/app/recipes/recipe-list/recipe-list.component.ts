4cc2515a4ddd312b5af94960e18bc9bc22b7ab15
melodiaz23 <meli.diaz23@gmail.com>
2023-06-13 05:49:58 -0500

    content added to recipes components

--- 5-shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 5-shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import {  Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/hmg-prod/images/casserole-recipes-6478d7f325a3a.jpeg')
+  ];
+
+  constructor(){
+
+  }
 
 }
