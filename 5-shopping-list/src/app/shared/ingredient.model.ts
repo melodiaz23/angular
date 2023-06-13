a493880319738827aa4b44138a2cca4e8524cc2a
melodiaz23 <meli.diaz23@gmail.com>
2023-06-13 18:21:58 -0500

    ingredient model shortcut

--- 5-shopping-list/src/app/shared/ingredient.model.ts
+++ 5-shopping-list/src/app/shared/ingredient.model.ts
@@ -1,9 +1,5 @@
-export class Ingredient{
-  public name: string;
-  public amount: number;
+export class Ingredient{ //shortcut
+  constructor(public name: string, public amount: number){
 
-  constructor(name: string, amount: number){
-    this.name = name; 
-    this.amount = amount;
   }
 }
