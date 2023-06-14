20a2855857563890842e063231eb9c43319bb7f4
melodiaz23 <meli.diaz23@gmail.com>
2023-06-14 13:12:57 -0500

    local refferences added on cookpit

--- 7-cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ 7-cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'], 
-  encapsulation: ViewEncapsulation.None // the attributes affects all the components
+  encapsulation: ViewEncapsulation.Emulated // Default
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement')  element: {type: string, name: string, content: string};
