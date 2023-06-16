2f1330635d130ccddf4f46b846d2c9b009a6a8f4
melodiaz23 <meli.diaz23@gmail.com>
2023-06-15 23:40:53 -0500

    buttons created

--- 8-cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ 8-cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent {
+  intervalId: any;
+  num: number = 0;
 
+  startEvent() {
+    this.intervalId = setInterval(() => {
+      this.num = this.num + 1;
+      console.log(this.num);
+    }, 1000);
+  }
+
+  stopEvent(){
+    clearInterval(this.intervalId);
+  }
 }
