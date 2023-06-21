32fb2db72b851ce4be7973929e75d1e7ede9f9dd
melodiaz23 <meli.diaz23@gmail.com>
2023-06-20 21:04:19 -0500

    assignment finished

--- 8-cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ 8-cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,17 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent {
-  intervalId: any;
+  @Output() eventCreated = new EventEmitter<number>();
   num: number = 0;
+  ref: any;
 
-  startEvent() {
-    this.intervalId = setInterval(() => {
-      this.num = this.num + 1;
-      console.log(this.num);
-    }, 1000);
+oddNumber: number[] = [];
+evenNumber: number[] = [];
+
+startEvent() {
+  this.ref = setInterval(() => {
+    this.num = this.num + 1;
+    console.log(this.num);
+    this.eventCreated.emit(+this.num);
+    this.newNumberGenerated(this.num)
+  }, 1000);
+}
+
+stopEvent(){
+  clearInterval(this.ref);
+}
+
+newNumberGenerated(num: number){
+  if (num % 2 === 0){
+    console.log('EVEN');
+    this.evenNumber.push(num);
+    console.log(this.evenNumber);
   }
 
-  stopEvent(){
-    clearInterval(this.intervalId);
+  if (num % 2 === 1){
+    console.log('ODD');
+    this.oddNumber.push(num);
+    console.log(this.oddNumber);
   }
+}
+
 }
